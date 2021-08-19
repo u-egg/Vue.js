3a415126fe3fdf177cdf306c3bcabf01746d9a9d
You jung hawn <dnemfdlemf@naver.com>
2021-08-19 09:40:51 +0900

    font

--- project/src/App.vue
+++ project/src/App.vue
@@ -15,4 +15,18 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+@font-face {
+  font-family: "Helvetica";
+  src: url("./assets/font/Helvetica.woff2") format("woff2"),
+    url("./assets/font/Helvetica.woff") format("woff");
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+article {
+  margin: 0 auto;
+  text-align: center;
+  font-family: Helvetica;
+}
+</style>
